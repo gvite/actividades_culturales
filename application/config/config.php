6e953ec91b48e03989299b148a6c912e249e8eab
Gerardo Flores <gflores@digitalartsnetwork.com.mx>
2016-08-05 00:01:59 -0500

    algunos cambios de configuracion local

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/actividades_culturales_nuevo/';
+$config['base_url']	= 'http://localhost/actividades_culturales/';
 
 /*
 |--------------------------------------------------------------------------
